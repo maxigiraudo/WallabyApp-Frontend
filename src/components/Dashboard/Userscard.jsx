329c4c02c0727d5dc286ddcf5a866ce8461949ec
Maxi Giraudo <92755973+maxigiraudo@users.noreply.github.com>
2022-07-26 20:43:49 -0300

    Update Userscard.jsx

--- src/components/Dashboard/Userscard.jsx
+++ src/components/Dashboard/Userscard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../Dashboard/Dashboard.module.css";
+
 import styles from "./Usercard.module.css";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
