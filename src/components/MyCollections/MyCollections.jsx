7bc048906dfb25de33c667033e8b1355885ab114
Maxi Giraudo <maximilianoezequielgiraudo@gmail.com>
2022-07-27 01:08:24 -0300

    push pa mati

--- src/components/MyCollections/MyCollections.jsx
+++ src/components/MyCollections/MyCollections.jsx
@@ -1,8 +1,7 @@
-
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import Navbar from "../Navbar/Navbar";
-import {useMoralis} from 'react-moralis'
+import { useMoralis } from "react-moralis";
 import styles from "./MyCollections.module.css";
 import { useNFTBalance } from "../../hooks/useNFTBalance";
 import Card from "../Card/Card";
@@ -10,17 +9,15 @@ import Footer from "../Footer/Footer";
 import { publishMarket } from "../../redux/actions";
 import { marketAddress, contractABI } from "../../contracts/contractMarket";
 import { useWeb3ExecuteFunction } from "react-moralis";
-import {Modal, Button} from 'react-bootstrap';
-
+import { Modal, Button } from "react-bootstrap";
 
 export default function MyCollections() {
-  
-  
   const { NFTBalance, fetchSuccess } = useNFTBalance();
-  const {Moralis} = useMoralis();
+  const { Moralis } = useMoralis();
   const [visible, setVisibility] = useState(false);
-  const [price, setPrice] = useState()
-  const [nftToSell, setNftToSell] = useState(null)
+  const [input, setInput] = useState(false);
+  const [price, setPrice] = useState();
+  const [nftToSell, setNftToSell] = useState(null);
   const contractProcessor = useWeb3ExecuteFunction();
   const contractABIJson = JSON.parse(JSON.stringify(contractABI));
   const listItemFunction = "createMarketItem";
@@ -28,51 +25,41 @@ export default function MyCollections() {
   async function list(nft, currentPrice) {
     const p = currentPrice * ("1e" + 18);
     const ops = {
-      contractAddress : marketAddress,
-      functionName : listItemFunction,
-      abi : contractABIJson,
-      params : {
+      contractAddress: marketAddress,
+      functionName: listItemFunction,
+      abi: contractABIJson,
+      params: {
         nftContract: nft.token_address,
-        tokenId : nft._id,
-        price :  String(p)
-      }
-
-      
+        tokenId: nft._id,
+        price: String(p),
+      },
     };
 
     await contractProcessor.fetch({
-      params : ops,
-      onSuccess : () => {
-        alert("item bougth")
+      params: ops,
+      onSuccess: () => {
+        alert("item bougth");
       },
-      onError : (e) => {
-        alert(e)
-
-      }
-    })
-
+      onError: (e) => {
+        alert(e);
+      },
+    });
   }
-
-
 
   const handleSellClick = (nft) => {
     setNftToSell(nft);
     setVisibility(true);
+    setInput(nft);
+  };
 
-  }
-
-  
-  console.log(NFTBalance)
-  
-  
+  console.log(NFTBalance);
 
   const back = () => {
     window.history.back();
   };
 
   const handleClose = () => setVisibility(false);
-  
-  
+
   return (
     <div>
       <Navbar />
@@ -87,55 +74,49 @@ export default function MyCollections() {
             <div className={styles.card} key={e.id}>
               <h3 className={styles.name}>{e.name}</h3>
               <button
-                onClick={() =>
-                  handleSellClick(e)
-                }
+                onClick={() => handleSellClick(e)}
                 className={styles.botoncito}
               >
                 Publish on the market
               </button>
+              {input ? <input className={styles.input} type="text" /> : null}
               <img className={styles.img} src={e.image} alt="img" />
             </div>
           ))}
-         
+
+          {/* <Modal
+            className={styles.ventanitaModal}
+            title={`Buy ${nftToSell?.name || "NFT"}`}
+            show={visible}
+            onHide={() => setVisibility(false)}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>{`Buy ${nftToSell?.name || "NFT"}`}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <img
+                src={nftToSell?.image}
+                style={{
+                  width: "250px",
+                  margin: "auto",
+                  borderRadius: "10px",
+                  marginBottom: "15px",
+                }}
+              />
+            </Modal.Body>
+            <Modal.Footer>
+              <input
+                autoFocus
+                placeholder="Set Price in Eth"
+                onChange={(e) => setPrice(e.target.value)}
+              />
+              <button onClick={() => list(nftToSell, price)}>List</button>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal> */}
         </div>
-        <Modal
-          title={`Buy ${nftToSell?.name || 'NFT'}`}
-          show={visible}
-          onHide={() => setVisibility(false)}
-          
-          
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>{`Buy ${nftToSell?.name || 'NFT'}`}</Modal.Title>
-
-          </Modal.Header>
-          <Modal.Body>
-          <img 
-            src={nftToSell?.image}
-            style={{
-              width: "250px",
-              margin: "auto",
-              borderRadius : "10px",
-              marginBottom : "15px"
-
-            }}
-          />
-
-          </Modal.Body>
-          <Modal.Footer>
-            <input  autoFocus placeholder="Set Price in Eth" onChange={e => setPrice(e.target.value)}/>
-            <button onClick={() => list(nftToSell, price)}>List</button>
-            <Button variant='secondary' onClick={handleClose}>
-              Close
-            </Button>
-          </Modal.Footer>
-          
-         
-
-        </Modal>
-        
-        
       </div>
       <Footer />
     </div>
